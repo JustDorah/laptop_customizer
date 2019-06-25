98dd1488b2d20a843a391957a7b2a78d3d990fc7
Dorah <dorah.bmw@gmail.com>
2019-06-25 01:56:22 -0500

    Added SummaryTotal.js

--- src/summary/Summary.js
+++ src/summary/Summary.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
+import SummaryTotal from "./SummaryTotal";
 
 export default class Summary extends Component {
   render() {
     return (
       <div>
         {this.props.summary}
-        <div className="summary__total">
-          <div className="summary__total__label">Your Price: </div>
-          <div className="summary__total__value">
-            {new Intl.NumberFormat("en-US", {
-              style: "currency",
-              currency: "USD"
-            }).format(this.props.total)}
-          </div>
-        </div>
+        <SummaryTotal total={this.props.total} />
       </div>
     );
   }
