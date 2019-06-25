fdb749818a11b2d1b11de4f68d51d86497a6de6b
Dorah <dorah.bmw@gmail.com>
2019-06-25 01:39:22 -0500

    Initial Commit App funtions separated out into different components

---
+++ src/NameHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class NameHeader extends Component {
+  render() {
+    return (
+      <header className="NameHeader">
+        <h1>ELF Computing</h1>
+        <h3>Laptops</h3>
+        <h5>Customize your laptop</h5>
+      </header>
+    );
+  }
+}
